a958e094899dad2a567937a6b18a8e379434add3
keithhedger <keithhedger@a077cb93-66c8-45fd-b335-998722470d8a>
2014-10-29 11:55:44 +0000

    done window version
    git-svn-id: file:///media/LinuxData/Development/SVN/XDecorations@80 a077cb93-66c8-45fd-b335-998722470d8a

--- xdecorations.cpp
+++ xdecorations.cpp
@@ -142,6 +142,16 @@ struct args
 	void*		data;
 };
 
+struct Hints
+{
+	unsigned long   flags;
+	unsigned long   functions;
+	unsigned long   decorations;
+	long            inputMode;
+	unsigned long   status;
+};
+
+
 enum {TYPEINT=1,TYPESTRING,TYPEBOOL};
 
 args	xdecorations_rc[]=
@@ -727,6 +737,9 @@ int main(int argc,char* argv[])
 	Window	root;
 	Window	parentWindow;
 	int		rc=0;
+	Hints	hints;
+	Atom	xa;
+	Atom	xa_prop[5];
 
 	prefix=strdup("Xmas");
 	asprintf(&configFilePath,"%s/.config/xdecorations.rc",getenv("HOME"));
@@ -860,37 +873,27 @@ int main(int argc,char* argv[])
 			rootWin=XCreateWindow(display,DefaultRootWindow(display),0,0,displayWidth,displayHeight,0,depth,InputOutput,visual,CWColormap | CWBorderPixel | CWBackPixel,&attr);
 			XSelectInput(display,rootWin,StructureNotifyMask);
 
-	Atom    				property;
-	Atom					xa;
-	Atom					xa_prop[10];
-	xa=XInternAtom(display,"_NET_WM_STATE",False);
-	xa_prop[0]=XInternAtom(display,"_NET_WM_STATE_STICKY",False);
-	xa_prop[1]=XInternAtom(display,"_NET_WM_STATE_BELOW",False);
-	xa_prop[2]=XInternAtom(display,"_NET_WM_STATE_SKIP_PAGER",False);
-	xa_prop[3]=XInternAtom(display,"_NET_WM_STATE_SKIP_TASKBAR",False);
-	xa_prop[4]=XInternAtom(display,"_NET_WM_STATE_FULLSCREEN",False);
+			xa=XInternAtom(display,"_NET_WM_STATE",False);
+			xa_prop[0]=XInternAtom(display,"_NET_WM_STATE_STICKY",False);
+			xa_prop[1]=XInternAtom(display,"_NET_WM_STATE_BELOW",False);
+			xa_prop[2]=XInternAtom(display,"_NET_WM_STATE_SKIP_PAGER",False);
+			xa_prop[3]=XInternAtom(display,"_NET_WM_STATE_SKIP_TASKBAR",False);
+			xa_prop[5]=XInternAtom(display,"_MOTIF_WM_HINTS",True);
 
-	xa=XInternAtom(display,"_NET_WM_STATE",False);
-	if(xa!=None)
-		XChangeProperty(display,rootWin,xa,XA_ATOM,32,PropModeAppend,(unsigned char *)&xa_prop,5);
+			xa=XInternAtom(display,"_NET_WM_STATE",False);
+			if(xa!=None)
+				XChangeProperty(display,rootWin,xa,XA_ATOM,32,PropModeAppend,(unsigned char *)&xa_prop,4);
 
-//	hints.flags=2;
-//	hints.decorations=0;
-//	property=XInternAtom(display,"_MOTIF_WM_HINTS",True);
-//			XChangeProperty(display,flyerWindow[j],property,property,32,PropModeReplace,(unsigned char *)&hints,5);
-
-
-
+			hints.flags=2;
+			hints.decorations=0;
+			XChangeProperty(display,rootWin,xa_prop[5],xa_prop[5],32,PropModeReplace,(unsigned char *)&hints,5);
 
 			gc=XCreateGC(display,rootWin,0,0);
+			rg=XCreateRegion();
+			XMoveWindow(display,rootWin,0,0);
+			XShapeCombineRegion(display,rootWin,ShapeInput,0,0,rg,ShapeSet);
 			XMapWindow(display,rootWin);
 			XSync(display, False);
-			rg=XCreateRegion();
-			XShapeCombineRegion(display,rootWin,ShapeInput,0,0,rg,ShapeSet);
-			XWindowAttributes xwa;
-			XGetWindowAttributes(display,DefaultRootWindow(display), &xwa);
-			XMoveResizeWindow(display,rootWin, 0, -900, xwa.width, xwa.height);
-printf("wid=%i h=%i\n",displayWidth,displayHeight);
 		}
 
 	initLamps();
@@ -913,7 +916,10 @@ printf("wid=%i h=%i\n",displayWidth,displayHeight);
 						if (ev.xclient.message_type == XInternAtom(display,"WM_PROTOCOLS",1) && (Atom)ev.xclient.data.l[0] == XInternAtom(display,"WM_DELETE_WINDOW",1))
 						done=1;
 						continue;
+						
 						break;
+					default:
+						XMoveWindow(display,rootWin,0,0);
 				}
 
 			usleep(mainDelay);
