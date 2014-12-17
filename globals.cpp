b3634b834dbea66430b56bb84f901f42fc8bff3c
keithhedger <keithhedger@a077cb93-66c8-45fd-b335-998722470d8a>
2014-12-17 11:44:03 +0000

    git-svn-id: file:///media/LinuxData/Development/SVN/XDecorations@170 a077cb93-66c8-45fd-b335-998722470d8a

--- globals.cpp
+++ globals.cpp
@@ -9,149 +9,149 @@
 
 #include "globals.h"
 
-char		pathname[MAXPATHNAMELEN];
-char*		configFilePath;
+char			pathname[MAXPATHNAMELEN];
+char			*configFilePath;
 
-Display*			display;
-Window				rootWin;
-int					displayWidth;
-int					displayHeight;
-GC					gc;
-GC					gcpm;
+Display*		display;
+Window			rootWin;
+int				displayWidth;
+int				displayHeight;
+GC				gc;
+GC				gcpm;
 
-Visual*				visual=NULL;
-int					depth=0;
-Imlib_Image			image;
-int					screen;
-Region				rg;
-XdbeBackBuffer		buffer;
-XdbeSwapInfo		swapInfo;
-Drawable			drawOnThis;
-bool				useBuffer=false;
-bool				useDBOveride=true;
+Visual*			visual=NULL;
+int				depth=0;
+Imlib_Image		image;
+int				screen;
+Region			rg;
+XdbeBackBuffer	buffer;
+XdbeSwapInfo	swapInfo;
+Drawable		drawOnThis;
+bool			useBuffer=false;
+bool			useDBOveride=true;
 
-int					done=0;
-long				mainDelay;
+int				done=0;
+long			mainDelay;
 
-uint				runCounter=0;
-bool				useWindow=true;
-int					offSetY=0;
-bool				watchConfig=false;
+uint			runCounter=0;
+bool			useWindow=true;
+int				offSetY=0;
+bool			watchConfig=false;
 
-char*				prefix;
+char*			prefix;
 
-bool				treeNeedsUpdate=false;
-bool				flyerNeedsUpdate=false;
-bool				figureNeedsUpdate=false;
-bool				fallingNeedsUpdate=false;
-bool				lampsNeedsUpdate=false;
+bool			treeNeedsUpdate=false;
+bool			flyerNeedsUpdate=false;
+bool			figureNeedsUpdate=false;
+bool			fallingNeedsUpdate=false;
+bool			lampsNeedsUpdate=false;
 
 //settled
-int					*down;
-Pixmap				settledPixmap;
-Pixmap				settledPixmapMask;
-int					settledHeight;
-int					settleRate;
-bool				keepSettling=true;
-int					blackColor;
-int					whiteColor;
-bool				clearOnMaxHeight;
+int				*down;
+Pixmap			settledPixmap;
+Pixmap			settledPixmapMask;
+int				settledHeight;
+int				settleRate;
+bool			keepSettling=true;
+int				blackColor;
+int				whiteColor;
+bool			clearOnMaxHeight;
 
 //falling
-objects				floating[MAXFLOAT];
-movement			moving[MAXFALLINGOBJECTS];
-int					fallingNumber=1;
-int					fallingDelay=1;
-int					swirlingDirection;
-int					numberOfFalling=100;
-bool				showFalling=true;
-int					fallingSpread=1000;
-int					fallSpeed=1;
-int					minFallSpeed=1;
-int					maxXStep=4;
-int					fallingAnimSpeed=8;
-int					fallingCount=0;
+objects			floating[MAXFLOAT];
+movement		moving[MAXFALLINGOBJECTS];
+int				fallingNumber=1;
+int				fallingDelay=1;
+int				swirlingDirection;
+int				numberOfFalling=100;
+bool			showFalling=true;
+int				fallingSpread=1000;
+int				fallSpeed=1;
+int				minFallSpeed=1;
+int				maxXStep=4;
+int				fallingAnimSpeed=8;
+int				fallingCount=0;
 
 //wind
-int					gustDuration=3000;
-int					gustEvent=10000;
-int					gustSpeed=40;
-int					realGustSpeed;
+int				gustDuration=3000;
+int				gustEvent=10000;
+int				gustSpeed=40;
+int				realGustSpeed;
 
-bool				useGusts=true;
-int					windSpeed=0;
-int					gustOffSet=0;
-int					gustPortion=1;
-double				gustInc;
-bool				doingGusts=false;
-double				incr=1.01;
+bool			useGusts=true;
+int				windSpeed=0;
+int				gustOffSet=0;
+int				gustPortion=1;
+double			gustInc;
+bool			doingGusts=false;
+double			incr=1.01;
 
-int					gustDirection=0;
-int					gustCountdown=0;
+int				gustDirection=0;
+int				gustCountdown=0;
 
 //flyers
-objects				flyers[MAXFLYER];
-movement			flyersMove[MAXNUMBEROFFLYERS];
-int					flyersSpeed=1;
-int					flyersStep=8;
-bool				showFlyers=false;
-int					flyersMaxY=400;
-int					flyerSpread=500;
-int					flyerCount=0;
-int					flyerAnimSpeed=8;
-int					flyerNumber;
-int					numberOfFlyers=1;
+objects			flyers[MAXFLYER];
+movement		flyersMove[MAXNUMBEROFFLYERS];
+int				flyersSpeed=1;
+int				flyersStep=8;
+bool			showFlyers=false;
+int				flyersMaxY=400;
+int				flyerSpread=500;
+int				flyerCount=0;
+int				flyerAnimSpeed=8;
+int				flyerNumber;
+int				numberOfFlyers=1;
 
 //figure
-Pixmap				figurePixmap[4];
-int					figureSpeed=100;
-int					figureX=100;
-int					figureY=100;
-int					figureW;
-int					figureH;
-int					figureOnOff=0;
-int					figureNumber=1;
+Pixmap			figurePixmap[4];
+int				figureSpeed=100;
+int				figureX=100;
+int				figureY=100;
+int				figureW;
+int				figureH;
+int				figureOnOff=0;
+int				figureNumber=1;
 
 //lamps
-Pixmap				lampsPixmap[4];
-int					lampSpeed=100;
-int					lampX=0;
-int					lampY=0;
-int					lampWidth;
-int					lampHeight;
-int					lampCount;
-int					lampSet=1;
-int					lampsOnOff=0;
-int					lampAnim;
-int					lastLampAnim;
-int					lampSection;
-int					lampCountdown;
-bool				*lampState;
-int					lampCycleDelay;
+Pixmap			lampsPixmap[4];
+int				lampSpeed=100;
+int				lampX=0;
+int				lampY=0;
+int				lampWidth;
+int				lampHeight;
+int				lampCount;
+int				lampSet=1;
+int				lampsOnOff=0;
+int				lampAnim;
+int				lastLampAnim;
+int				lampSection;
+int				lampCountdown;
+bool			*lampState;
+int				lampCycleDelay;
 
 //trees
-Pixmap				treePixmap[2];
-int					treeWidth;
-int					treeHeight;
-int					treeNumber=1;
-int					treelampSpeed=100;
-int					starSpeed=100;
-int					treeX=0;
-int					treeY=0;
-int					treeLampSet=1;
-int					treeOnOff=0;
-bool				showTree=true;
+Pixmap			treePixmap[2];
+int				treeWidth;
+int				treeHeight;
+int				treeNumber=1;
+int				treelampSpeed=100;
+int				starSpeed=100;
+int				treeX=0;
+int				treeY=0;
+int				treeLampSet=1;
+int				treeOnOff=0;
+bool			showTree=true;
 
 //tinsel`
-Pixmap				tinselPixmap[2];
-bool				showTinsel=false;
+Pixmap			tinselPixmap[2];
+bool			showTinsel=false;
 
 //star
-Pixmap				starPixmap[4];
-bool				showStar=false;
-int					starOnOff=0;
+Pixmap			starPixmap[4];
+bool			showStar=false;
+int				starOnOff=0;
 
 //tree lamps
-Pixmap				treeLampsPixmap[MAXNUMBEROFTREELIGHTS][2];
-bool				showTreeLamps=false;
-int					treeLampCount=0;
+Pixmap			treeLampsPixmap[MAXNUMBEROFTREELIGHTS][2];
+bool			showTreeLamps=false;
+int				treeLampCount=0;
--- globals.h
+++ globals.h
@@ -37,30 +37,30 @@
 #define VERSION "0.1.5"
 #define _SELECTPIXMAP(a,b) (a+(2*b))//a=ONPIXMAP b=xxxOnOff
 
-struct				objects
+struct					objects
 {
-	Pixmap*			pixmap[MAXFALLINGANIMATION];
-	Pixmap*			mask[MAXFALLINGANIMATION];
-	int				h[MAXFALLINGANIMATION];
-	int				w[MAXFALLINGANIMATION];
-	int				anims;
+	Pixmap*				pixmap[MAXFALLINGANIMATION];
+	Pixmap*				mask[MAXFALLINGANIMATION];
+	int					h[MAXFALLINGANIMATION];
+	int					w[MAXFALLINGANIMATION];
+	int					anims;
 };
 
-struct				movement
+struct					movement
 {
-	objects			*object;
-	int				x;
-	int				y;
-	int				deltaX;
-	int				deltaY;
-	int				stepX;
-	int				stepY;
-	bool			use;
-	int				imageNum;
-	int				countDown;
-	int				maxW;
-	int				maxH;
-	bool			direction;
+	objects				*object;
+	int					x;
+	int					y;
+	int					deltaX;
+	int					deltaY;
+	int					stepX;
+	int					stepY;
+	bool				use;
+	int					imageNum;
+	int					countDown;
+	int					maxW;
+	int					maxH;
+	bool				direction;
 };
 
 enum {ONPIXMAP=0,ONMASK,OFFPIXMAP,OFFMASK};
@@ -68,40 +68,40 @@ enum {LEFT=-1000,CENTRE=-2000,RIGHT=-3000,TOP=-4000,BOTTOM=-5000};
 enum {TYPEINT=1,TYPESTRING,TYPEBOOL};
 enum {LAMPFLASH=0,INVERTLAMPCHASE,LAMPCHASE,LAMPRANDOM,LAMPCYCLE,NUMOPTIONS};
 
-extern char		pathname[MAXPATHNAMELEN];
-extern char*		configFilePath;
+extern char				pathname[MAXPATHNAMELEN];
+extern char				*configFilePath;
 
 extern Display*			display;
-extern Window				rootWin;
-extern int					displayWidth;
-extern int					displayHeight;
-extern GC					gc;
-extern GC					gcpm;
+extern Window			rootWin;
+extern int				displayWidth;
+extern int				displayHeight;
+extern GC				gc;
+extern GC				gcpm;
 
-extern Visual*				visual;
-extern int					depth;
-extern Imlib_Image			image;
-extern int					screen;
-extern Region				rg;
-extern XdbeBackBuffer		buffer;
+extern Visual*			visual;
+extern int				depth;
+extern Imlib_Image		image;
+extern int				screen;
+extern Region			rg;
+extern XdbeBackBuffer	buffer;
 extern XdbeSwapInfo		swapInfo;
 extern Drawable			drawOnThis;
 extern bool				useBuffer;
 extern bool				useDBOveride;
 
-extern int					done;
+extern int				done;
 extern long				mainDelay;
 
 extern uint				runCounter;
 extern bool				useWindow;
-extern int					offSetY;
+extern int				offSetY;
 extern bool				watchConfig;
 extern uint				runCounter;
 extern bool				useWindow;
-extern int					offSetY;
+extern int				offSetY;
 extern bool				watchConfig;
 
-extern char*				prefix;
+extern char				*prefix;
 
 extern bool				treeNeedsUpdate;
 extern bool				flyerNeedsUpdate;
@@ -110,113 +110,113 @@ extern bool				fallingNeedsUpdate;
 extern bool				lampsNeedsUpdate;
 
 //settled
-extern int					*down;
-extern Pixmap				settledPixmap;
-extern Pixmap				settledPixmapMask;
-extern int					settledHeight;
-extern int					settleRate;
+extern int				*down;
+extern Pixmap			settledPixmap;
+extern Pixmap			settledPixmapMask;
+extern int				settledHeight;
+extern int				settleRate;
 extern bool				keepSettling;
-extern int					blackColor;
-extern int					whiteColor;
+extern int				blackColor;
+extern int				whiteColor;
 extern bool				clearOnMaxHeight;
 
 //falling
-extern objects				floating[MAXFLOAT];
+extern objects			floating[MAXFLOAT];
 extern movement			moving[MAXFALLINGOBJECTS];
-extern int					fallingNumber;
-extern int					fallingDelay;
-extern int					swirlingDirection;
-extern int					numberOfFalling;
+extern int				fallingNumber;
+extern int				fallingDelay;
+extern int				swirlingDirection;
+extern int				numberOfFalling;
 extern bool				showFalling;
-extern int					fallingSpread;
-extern int					fallSpeed;
-extern int					minFallSpeed;
-extern int					maxXStep;
-extern int					fallingAnimSpeed;
-extern int					fallingCount;
+extern int				fallingSpread;
+extern int				fallSpeed;
+extern int				minFallSpeed;
+extern int				maxXStep;
+extern int				fallingAnimSpeed;
+extern int				fallingCount;
 
 //wind
-extern int					gustDuration;
-extern int					gustEvent;
-extern int					gustSpeed;
-extern int					realGustSpeed;
+extern int				gustDuration;
+extern int				gustEvent;
+extern int				gustSpeed;
+extern int				realGustSpeed;
 
 extern bool				useGusts;
-extern int					windSpeed;
-extern int					gustOffSet;
-extern int					gustPortion;
-extern double				gustInc;
+extern int				windSpeed;
+extern int				gustOffSet;
+extern int				gustPortion;
+extern double			gustInc;
 extern bool				doingGusts;
-extern double				incr;
+extern double			incr;
 
-extern int					gustDirection;
-extern int		gustCountdown;
+extern int				gustDirection;
+extern int				gustCountdown;
 
 //flyers
-extern objects	flyers[MAXFLYER];
-extern movement	flyersMove[MAXNUMBEROFFLYERS];
-extern int					flyersSpeed;
-extern int					flyersStep;
+extern objects			flyers[MAXFLYER];
+extern movement			flyersMove[MAXNUMBEROFFLYERS];
+extern int				flyersSpeed;
+extern int				flyersStep;
 extern bool				showFlyers;
-extern int					flyersMaxY;
-extern int					flyerSpread;
-extern int					flyerCount;
-extern int					flyerAnimSpeed;
-extern int					flyerNumber;
-extern int					numberOfFlyers;
+extern int				flyersMaxY;
+extern int				flyerSpread;
+extern int				flyerCount;
+extern int				flyerAnimSpeed;
+extern int				flyerNumber;
+extern int				numberOfFlyers;
 
 //figure
-extern Pixmap				figurePixmap[4];
-extern int					figureSpeed;
-extern int					figureX;
-extern int					figureY;
-extern int					figureW;
-extern int					figureH;
-extern int					figureOnOff;
-extern int					figureNumber;
+extern Pixmap			figurePixmap[4];
+extern int				figureSpeed;
+extern int				figureX;
+extern int				figureY;
+extern int				figureW;
+extern int				figureH;
+extern int				figureOnOff;
+extern int				figureNumber;
 
 //lamps
-extern Pixmap				lampsPixmap[4];
-extern int					lampSpeed;
-extern int					lampX;
-extern int					lampY;
-extern int					lampWidth;
-extern int					lampHeight;
-extern int					lampCount;
-extern int					lampSet;
-extern int					lampsOnOff;
-extern int					lampAnim;
-extern int					lastLampAnim;
-extern int					lampSection;
-extern int					lampCountdown;
+extern Pixmap			lampsPixmap[4];
+extern int				lampSpeed;
+extern int				lampX;
+extern int				lampY;
+extern int				lampWidth;
+extern int				lampHeight;
+extern int				lampCount;
+extern int				lampSet;
+extern int				lampsOnOff;
+extern int				lampAnim;
+extern int				lastLampAnim;
+extern int				lampSection;
+extern int				lampCountdown;
 extern bool				*lampState;
-extern int					lampCycleDelay;
+extern int				lampCycleDelay;
 
 //trees
-extern Pixmap				treePixmap[2];
-extern int					treeWidth;
-extern int					treeHeight;
-extern int					treeNumber;
-extern int					treelampSpeed;
-extern int					starSpeed;
-extern int					treeX;
-extern int					treeY;
-extern int					treeLampSet;
-extern int					treeOnOff;
+extern Pixmap			treePixmap[2];
+extern int				treeWidth;
+extern int				treeHeight;
+extern int				treeNumber;
+extern int				treelampSpeed;
+extern int				starSpeed;
+extern int				treeX;
+extern int				treeY;
+extern int				treeLampSet;
+extern int				treeOnOff;
 extern bool				showTree;
 
 //tinsel
-extern Pixmap				tinselPixmap[2];
+extern Pixmap			tinselPixmap[2];
 extern bool				showTinsel;
 
 //star
-extern Pixmap				starPixmap[4];
+extern Pixmap			starPixmap[4];
 extern bool				showStar;
-extern int					starOnOff;
+extern int				starOnOff;
 
 //tree lamps
-extern Pixmap				treeLampsPixmap[MAXNUMBEROFTREELIGHTS][2];
+extern Pixmap			treeLampsPixmap[MAXNUMBEROFTREELIGHTS][2];
 extern bool				showTreeLamps;
-extern int					treeLampCount;
+extern int				treeLampCount;
 
 #endif
